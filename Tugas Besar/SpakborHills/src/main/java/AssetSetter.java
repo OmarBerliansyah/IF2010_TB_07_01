a8e7f3a76011efa2dc95b9f9b0d7e367c0e470d3
aliyaharta <167730631+aliyaharta@users.noreply.github.com>
2025-05-18 00:59:55 +0700

    atur yaAllah

---
+++ Tugas Besar/SpakborHills/src/main/java/AssetSetter.java
@@ -0,0 +1,42 @@
+package main.java;
+
+import entity.NPC_1;
+import objects.OBJ_Chest;
+import objects.OBJ_Door;
+import objects.OBJ_Wood;
+
+public class AssetSetter {
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp){
+        this.gp = gp;
+    }
+
+    public void setObject(){
+        gp.obj[0] = new OBJ_Door();
+        gp.obj[0].worldX = 23 * gp.tileSize;
+        gp.obj[0].worldY = 7 * gp.tileSize;
+
+        gp.obj[1] = new OBJ_Chest();
+        gp.obj[1].worldX = 23 * gp.tileSize;
+        gp.obj[1].worldY = 8 * gp.tileSize;
+
+        gp.obj[2] = new OBJ_Wood();
+        gp.obj[2].worldX = 22 * gp.tileSize;
+        gp.obj[2].worldY = 7 * gp.tileSize;
+
+        gp.obj[3] = new OBJ_Wood();
+        gp.obj[3].worldX = 20 * gp.tileSize;
+        gp.obj[3].worldY = 7 * gp.tileSize;
+
+        gp.obj[4] = new OBJ_Wood();
+        gp.obj[4].worldX = 18 * gp.tileSize;
+        gp.obj[4].worldY = 7 * gp.tileSize;
+    }
+
+    public void setNPC(){
+        gp.NPC[0] = new NPC_1(gp);
+        gp.NPC[0].worldX = gp.tileSize*21;
+        gp.NPC[0].worldY = gp.tileSize*21;
+    }
+}
